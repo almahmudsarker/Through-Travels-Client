8fa5e862e118772fc7e1202b1b9316b490df02bd
almahmudsarker <almahmuds427@gmail.com>
2023-08-01 02:12:01 +0600

    places & hotels switching part done

--- src/components/Categories/CategoryBox.jsx
+++ src/components/Categories/CategoryBox.jsx
@@ -1,8 +1,28 @@
 import React from 'react';
-
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import qs from 'query-string'
 const CategoryBox = ({label, icon:Icon}) => {
+    const [params, setParams] = useSearchParams({})
+    const navigate = useNavigate()
+
+    const handleClick = () => {
+        let currentQuery = {}
+        if (params){
+            currentQuery = qs.parse(params.toString())
+        }
+        const updatedQuery = {
+            ...currentQuery, category: label,
+        }
+
+        const url = qs.stringifyUrl({
+            url:'/',
+            query: updatedQuery
+        }, {skipNull: true})
+
+        navigate(url)
+    }
     return (
-        <div className='flex flex-col items-center justify-center gap-2 p-3 border-b-2 hover:text-neutral-800 border-transparent text-neutral-500'>
+        <div onClick={handleClick} className='flex cursor-pointer flex-col items-center justify-center gap-2 p-3 border-b-2 hover:text-neutral-800 border-transparent text-neutral-500'>
             <Icon size={26} />
             <div className="text-sm font-medium">{label}</div>
         </div>
--- src/components/Places/Places.jsx
+++ src/components/Places/Places.jsx
@@ -3,20 +3,31 @@ import { useEffect } from 'react';
 import Container from '../Shared/Container';
 import Card from './Card';
 import Loader from '../Shared/Loader';
+import { useSearchParams } from 'react-router-dom';
+import Heading from '../Heading/Heading';
 
 const Places = () => {
+    const [params, setParams] = useSearchParams({});
+    const category = params.get("category");
+
     const [places, setPlaces] = useState([])
     const [loading, setLoading] = useState(false)
+
     useEffect(() => {
-        setLoading(true)
-        fetch('./places.json')
-        .then(res => res.json())
-        .then(data => {
-            setPlaces(data)
-            setLoading(false)
+      setLoading(true);
+      fetch("./places.json")
+        .then((res) => res.json())
+        .then((data) => {
+            if (category){
+                const filtered = data.filter((place) => place.category === category)
+                setPlaces(filtered)
+            }else{
+                setPlaces(data);
+            }
+          setLoading(false);
         })
-        .catch(err => console.log(err))
-    }, [])
+        .catch((err) => console.log(err));
+    }, [category]);
 
     if (loading) {
         return <Loader />
@@ -24,11 +35,22 @@ const Places = () => {
 
     return (
         <Container>
-            <div className="pt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+            {
+                places && places.length > 0 ? (
+                    <div className="pt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
                 {
                     places.map((place, index) => (<Card key={index} place={place} />))
                 }
             </div>
+                ) : (
+                    <div className="pt-12">
+                        <Heading 
+                        title="No Places Available In This Category" 
+                        subtitle="Please try another category" 
+                        center={true} />
+                    </div>
+                )
+            }
         </Container>
     );
 };
