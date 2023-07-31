cc9f29058c370798a8e624faeea549e2b45cb459
almahmudsarker <almahmuds427@gmail.com>
2023-08-01 00:24:19 +0600

    registration part done

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom'
 import Main from '../layouts/Main'
 import Home from '../pages/Home/Home'
 import Login from '../pages/Login/Login'
+import SignUp from '../pages/Signup/SignUp'
 
 export const router = createBrowserRouter([
   {
@@ -15,5 +16,9 @@ export const router = createBrowserRouter([
   {
     path: '/login',
     element: <Login />,
+  },
+  {
+    path: '/signup',
+    element: <SignUp />,
   },
 ])
