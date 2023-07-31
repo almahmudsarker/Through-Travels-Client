5e678b4209956f873b77b4567610bf7faba2632e
almahmudsarker <almahmuds427@gmail.com>
2023-07-31 22:54:41 +0600

    login part done

---
+++ src/components/Categories/Categories.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Container from '../Shared/Container';
+import { categories } from './categoriesData';
+import CategoryBox from './CategoryBox';
+const Categories = () => {
+    return (
+        <div>
+            <Container>
+                <div className='pt-4 flex flex-row items-center justify-between overflow-x-auto'>
+                    {
+                        categories.map(item => <CategoryBox key={item.label} label={item.label} icon={item.icon} />)
+                    }
+                </div>
+            </Container>
+        </div>
+    );
+};
+
+export default Categories;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,9 +1,19 @@
 import { createBrowserRouter } from 'react-router-dom'
 import Main from '../layouts/Main'
+import Home from '../pages/Home/Home'
+import Login from '../pages/Login/Login'
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <Main />,
+    children: [{
+      path: '/',
+      element: <Home />,
+    }]
+  },
+  {
+    path: '/login',
+    element: <Login />,
   },
 ])
